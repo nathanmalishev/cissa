65d09f1d4fdd3f4d3b6c70785fde4dba01e0dab7
Nathan Malishev <nathan.malishev@gmail.com>
2016-02-14 18:36:37 +1100

    testing on heroku

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine','handlebars');
 
 //allows us to use the static game
-app.use(express.static(path.normalize(__dirname,'/flappy-bird' )));
-app.use(express.static(path.join(__dirname,'/views' )));
 
 
 //allow server to recieve json objects on POST
