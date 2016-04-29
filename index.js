01f5f26c09eaeb35c44625f317df2978b07d7d0d
Nathan Malishev <nathan.malishev@gmail.com>
2016-04-29 15:44:58 +1000

    Config dictates port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var app = require('./server/server');
+var config = require('./server/config/config');
 
-
-app.listen(3000);
+/* start server */
+app.listen(config.port);
+console.log('Server started on '+config.port)
